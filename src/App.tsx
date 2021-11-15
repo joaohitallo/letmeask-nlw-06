6755a4eeb6c862bcde36a9d0d8cf8cd5ebe55af2
joao Hitallo <joaohitallo97@gmail.com>
2021-11-15 05:12:14 -0300

    adicionando Autenticaçao com firebase por context

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import {
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom"
+import { AuthContextProvider } from './contexts/AuthContext'
 
 
 function App() {
+
+
   return (
     <BrowserRouter>
-      <Route  exact path="/" component={Home}/>
-      <Route  exact path="/rooms/new" component={NewRoom}/>
+      <AuthContextProvider>
+        <Route  exact path="/" component={Home}/>
+        <Route  exact path="/rooms/new" component={NewRoom}/>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
