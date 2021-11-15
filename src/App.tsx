54de010798c716a0e3d36b956fafdd3bc66b19bd
joao Hitallo <joaohitallo97@gmail.com>
2021-11-15 04:08:16 -0300

    adicionando Rotas com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import {
+  BrowserRouter,
+  Route,
+} from "react-router-dom";
+
 import { Home } from "./pages/Home";
-import { NewRoom} from './pages/NewRoom'
+import { NewRoom } from "./pages/NewRoom"
 
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route  exact path="/" component={Home}/>
+      <Route  exact path="/rooms/new" component={NewRoom}/>
+    </BrowserRouter>
   );
 }
 
